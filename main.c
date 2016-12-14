c1daf808bb37181c5658cc380e5a8cce076fbce7
ZhengPengqiao <157510312@qq.com>
2016-12-14 09:16:27 +0800

    将必要的参数使用宏来代替，这样有利于以后参数的更改

--- main.c
+++ main.c
@@ -2,13 +2,46 @@
 
 This example reads from the default PCM device
 and writes to standard output for 5 seconds of data.
-
 */
 
 /* Use the newer ALSA API */
 #define ALSA_PCM_NEW_HW_PARAMS_API
 
+
+/*
+样本长度(sample)：样本是记录音频数据最基本的单位，常见的有8位和16位。
+
+通道数(channel)：该参数为1表示单声道，2则是立体声。
+
+桢(frame)：桢记录了一个声音单元，其长度为样本长度与通道数的乘积。
+
+采样率(rate)：每秒钟采样次数，该次数是针对桢而言。
+
+周期(period)：音频设备一次处理所需要的桢数，对于音频设备的数据访问以及音频数据的存储，都是以此为单
+位。
+
+交错模式(interleaved)：是一种音频数据的记录方式，在交错模式下，数据以连续桢的形式存放，即首先记录
+完桢1的左声道样本和右声道样本（假设为立体声格式），再开始桢2的记录。而在非交错模式下，首先记录的是一
+个周期内所有桢的左声道样本，再记录右声道样本，数据是以连续通道的方式存储。不过多数情况下，我们只需要
+使用交错模式就可以了。
+
+
+period(周期):硬件中中断间的间隔时间。它表示输入延时。
+声卡接口中有一个指针来指示声卡硬件缓存区中当前的读写位置。只要接口在运行，这个指针将循环地指向缓存区
+中的某个位置。
+frame size = sizeof(one sample) * nChannels
+alsa中配置的缓存(buffer)和周期(size)大小在runtime中是以帧(frames)形式存储的。
+period_bytes = frames_to_bytes(runtime, runtime->period_size);
+bytes_to_frames()
+
+The period and buffer sizes are not dependent on the sample format because they are measured in frames; you do not need to change them.
+*/
 #include <alsa/asoundlib.h>
+
+#define    M_AUDIO_CHANNELS        1       //音频通道数量，必须固定是1，不然代码需要修改
+#define    M_AUDIO_SAMPLERATE      8000    //音频采样频率，11025、22050、44100等
+#define    M_AUDIO_BITSPERSAMPLE   SND_PCM_FORMAT_S16_LE//音频样本位数，必须固定是16
+#define    FRAMES                  505           //每周期的帧数
 
 int main() {
     long loops;
@@ -23,8 +56,7 @@ int main() {
 
     /*以录制模式打开*/
     /* Open PCM device for recording (capture). */
-    rc = snd_pcm_open(&handle, "default",
-                        SND_PCM_STREAM_CAPTURE, 0);
+    rc = snd_pcm_open(&handle, "default", SND_PCM_STREAM_CAPTURE, 0);
     if (rc < 0) {
         fprintf(stderr,
                 "unable to open pcm device: %s/n",
@@ -44,27 +76,23 @@ int main() {
 
     /*交错模式*/
     /* Interleaved mode */
-    snd_pcm_hw_params_set_access(handle, params,
+    //snd_pcm_hw_params_set_access(handle, params, \
                           SND_PCM_ACCESS_RW_INTERLEAVED);
 
-    /*PCM格式*/
-    /* Signed 16-bit little-endian format */
+    /*PCM格式 M_AUDIO_BITSPERSAMPLE*/
     snd_pcm_hw_params_set_format(handle, params,
-                                  SND_PCM_FORMAT_S16_LE);
+                                  M_AUDIO_BITSPERSAMPLE);
 
-    /*设置通道数*/
-    /* Two channels (stereo) */
-    snd_pcm_hw_params_set_channels(handle, params, 2);
+    /*设置通道数M_AUDIO_CHANNELS*/
+    snd_pcm_hw_params_set_channels(handle, params, M_AUDIO_CHANNELS);
 
     /*设置采样率*/
-    /* 44100 bits/second sampling rate (CD quality) */
-    val = 44100;
+    val = M_AUDIO_SAMPLERATE;
     snd_pcm_hw_params_set_rate_near(handle, params,
                                 &val, &dir);
 
-    /*没周期的帧数*/
-    /* Set period size to 32 frames. */
-    frames = 32;
+    /*每周期的帧数*/
+    frames = FRAMES;
     snd_pcm_hw_params_set_period_size_near(handle,
                             params, &frames, &dir);
 
@@ -80,7 +108,8 @@ int main() {
     /* Use a buffer large enough to hold one period */
     snd_pcm_hw_params_get_period_size(params,
                                           &frames, &dir);
-    size = frames * 4; /* 2 bytes/sample, 2 channels */
+    /* 2 bytes/sample,  M_AUDIO_CHANNELS channels */
+    size = frames * 2 * M_AUDIO_CHANNELS; 
     buffer = (char *) malloc(size);
 
     /* We want to loop for 5 seconds */
